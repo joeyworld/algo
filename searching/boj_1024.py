53f10b96f74197fc519f6737fc296f491aaa0490
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-12-10 15:20:31 +0900

    pep 8에 맞게 수정

--- searching/boj_1024.py
+++ searching/boj_1024.py
@@ -1,23 +1,23 @@
 def solve(total, length):
     while length < 101:
-            base = total // length
-            flag = [False, False]
-            while True:
-                seq = [(base + i) for i in range(length)]
-                sum_of_seq = sum(seq)
-                if sum_of_seq == total:
-                    return seq
-                elif sum_of_seq > total:
-                    base -= 1
-                    flag[1] = True
-                    if base < 0:
-                        break
-                else:
-                    base += 1
-                    flag[0] = True
-                if flag == [True, True]:
+        base = total // length
+        flag = [False, False]
+        while True:
+            seq = [(base + i) for i in range(length)]
+            sum_of_seq = sum(seq)
+            if sum_of_seq == total:
+                return seq
+            elif sum_of_seq > total:
+                base -= 1
+                flag[1] = True
+                if base < 0:
                     break
-            length += 1
+            else:
+                base += 1
+                flag[0] = True
+            if flag == [True, True]:
+                break
+        length += 1
     return None
 
 
